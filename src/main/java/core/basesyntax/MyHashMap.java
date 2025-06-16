2bbf49367f03b83bc8bf2a646bf34959a6f08c38
User <n0938951561@gmail.com>
2025-06-16 16:08:49 +0200

    created resized, node, override equals, hashcode, define put, getvalue, getsize

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,79 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private int capacity = 16;
+    private float loadFactor = 0.75f;
+    private int threshold = (int) (capacity * loadFactor);
+    private Node<K, V>[] table = new Node[capacity];
+    private int size;
 
     @Override
     public void put(K key, V value) {
+        int hash = key == null ? 0 : key.hashCode();
+        int index = hash & (capacity - 1);
 
+        Node<K, V> current = table[index];
+
+        while (current != null) {
+            if (Objects.equals(current.key, key)) {
+                current.value = value;
+                return;
+            }
+            current = current.next;
+        }
+
+        Node<K, V> newNode = new Node<>(key, value, table[index]);
+        table[index] = newNode;
+        size++;
+
+        if (size > capacity * loadFactor) {
+            resize();
+        }
     }
 
     @Override
     public V getValue(K key) {
+        int hash = key == null ? 0 : key.hashCode();
+        int index = hash & (capacity - 1);
+
+        Node<K, V> current = table[index];
+
+        while (current != null) {
+            if (Objects.equals(current.key, key)) {
+                return current.value;
+            }
+            current = current.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
     }
+
+    private void resize() {
+        int newCapacity = capacity * 2;
+        Node<K, V>[] newTable = new Node[newCapacity];
+
+        for (int i = 0; i < table.length; i++) {
+            Node<K, V> current = table[i];
+            while (current != null) {
+                Node<K, V> next = current.next;
+
+                int newIndex = current.key.hashCode() % newCapacity;
+                current.next = newTable[newIndex];
+                newTable[newIndex] = current;
+
+                current = next;
+            }
+        }
+
+        table = newTable;
+        capacity = newCapacity;
+        threshold = (int)(capacity * loadFactor);
+    }
+
 }
---
+++ src/main/java/core/basesyntax/Node.java
@@ -0,0 +1,30 @@
+package core.basesyntax;
+
+import java.util.Objects;
+
+public class Node<K, V> {
+    final K key;
+    V value;
+    Node<K, V> next;
+
+    Node (K key, V value, Node<K, V> next) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Node<K,V> node = (Node<K, V>) obj;
+        return Objects.equals(key, node.key) && Objects.equals(value, node.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
+}
